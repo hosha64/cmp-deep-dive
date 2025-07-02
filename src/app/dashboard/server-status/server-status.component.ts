583b0f33d2d3a5ebc3eb9c179c2771002e106fbf
Hossein shabani <h_shabani@isc.co.ir>
2025-07-02 17:16:14 +0330

    new version

---
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.scss'
+})
+export class ServerStatusComponent {
+
+}
---
+++ src/app/dashboard/traffic/traffic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic',
+  imports: [],
+  templateUrl: './traffic.component.html',
+  styleUrl: './traffic.component.scss'
+})
+export class TrafficComponent {
+
+}
