ca6fd833af778899d3cf9426c17c59084ec43516
Hossein shabani <h_shabani@isc.co.ir>
2025-07-02 17:56:18 +0330

    shabani

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TicketsComponent,
+    TrafficComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cmp-deep-dive';
